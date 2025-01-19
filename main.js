b89f3e2c1af8265679a08b9cde6aed5a05877640
ibnu naufal <ibnunaufal5758@gmail.com>
2025-01-19 17:41:44 +0700

    until create gradle file

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
+    fullscreenable: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -30,14 +31,53 @@ app.on('activate', () => {
   }
 });
 
-ipcMain.on('select-multiple-files', async (event) => {
+ipcMain.on('select-google-json-file', async (event) => {
+  const result = await dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [
+      { name: 'JSON Files', extensions: ['json'] }
+    ]
+  });
+  event.reply('google-json-file-selected', result.filePaths[0]);
+});
+
+ipcMain.on('select-multi-file', async (event) => {
   const result = await dialog.showOpenDialog({
     properties: ['openFile', 'multiSelections'],
     filters: [
       { name: 'JSON Files', extensions: ['json'] }
     ]
   });
-  event.reply('multiple-files-selected', result.filePaths);
+  event.reply('google-json-file-selected', result.filePaths);
+});
+
+ipcMain.on('select-flavor-file', async (event) => {
+  try {
+    const result = await dialog.showOpenDialog({
+      properties: ['openFile'],
+      filters: [
+        { name: 'JSON Files', extensions: ['json'] }
+      ]
+    });
+    
+    if (!result.canceled && result.filePaths.length > 0) {
+      const filePath = result.filePaths[0];
+      const fileContent = await fs.readFile(filePath, 'utf8');
+      
+      try {
+        // Verify if it's valid JSON
+        const jsonContent = JSON.parse(fileContent);
+        event.reply('flavor-file-selected', {
+          path: filePath,
+          content: jsonContent
+        });
+      } catch (jsonError) {
+        event.reply('json-error', 'Invalid JSON file');
+      }
+    }
+  } catch (error) {
+    event.reply('json-error', `Error reading file: ${error.message}`);
+  }
 });
 
 ipcMain.on('select-single-file', async (event) => {
@@ -69,11 +109,21 @@ ipcMain.on('select-single-file', async (event) => {
   }
 });
 
-ipcMain.on('select-directory', async (event) => {
+ipcMain.on('select-directory-android-studio', async (event) => {
   const result = await dialog.showOpenDialog({
     properties: ['openDirectory']
   });
-  event.reply('directory-selected', result.filePaths[0]);
+  event.reply('directory-selected-android-studio', result.filePaths[0]);
+  let gradleFile = path.join(result.filePaths[0], 'app/build.gradle');
+  let gradleFileContent = await fs.readFile(gradleFile, 'utf8');
+  event.reply('build-gradle-content-detected', gradleFileContent);
+});
+
+ipcMain.on('select-directory-result', async (event) => {
+  const result = await dialog.showOpenDialog({
+    properties: ['openDirectory']
+  });
+  event.reply('directory-selected-result', result.filePaths[0]);
 });
 
 ipcMain.on('run-script', (event) => {
@@ -100,5 +150,28 @@ ipcMain.on('create-folder', async (event) => {
     event.reply('folder-created', 'Folder created successfully');
   } catch (error) {
     event.reply('folder-error', `Error creating folder: ${error.message}`);
+  }
+});
+
+ipcMain.on('create-file', async (event, data) => {
+  try {
+    const filePath = path.join(data.path, data.fileName);
+    await fs
+      .writeFileSync(filePath, data.content, 'utf8');
+    event.reply('file-created', 'File created successfully');
+  } catch (error) {
+    event.reply('file-error', `Error creating file: ${error.message}`);
+  }
+});
+
+ipcMain.on('coba', async (event) => {
+  try {
+    let t = localStorage.getItem('test');
+    console.log(t);
+    // /Users/macbookpro/Downloads/coba
+    const filePath = '/Users/macbookpro/Downloads/coba/test.txt';
+    await fs.writeFile(filePath, 'Hello World', 'utf8');
+  } catch (error) {
+    ;
   }
 });
